9b0fc5b53dfe46d0de13b22dd2f3c5d0bab1eaf4
Mithun Biswas <mithunbiswas.me@gmail.com>
2026-09-22 14:35:11 +0600

    Add Impact tracking script to head

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -91,6 +91,14 @@ export default function RootLayout({ children }) {
             {STATIC_SETTINGS.google_tag_manager}
           </Script>
         )}
+
+        {/* Impact Tracking Script */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7824552-063d-440e-a0fd-c4d41aaac8961.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`,
+          }}
+        />
       </head>
 
       <body
